c09b54cb95ada1c0bdcacf5767f491d77e6e1cba
FennEXE <luuknagtegaal@gmail.com>
2020-11-17 13:37:55 +0100

    Update script.js
    Added 8d6 dice with sorted array

--- Opdracht 10 - JavaScript dobbelstenen/script.js
+++ Opdracht 10 - JavaScript dobbelstenen/script.js
@@ -1,19 +1,42 @@
-var numbertoword = ["Een", "Twee", "Drie", "Vier", "Vijf", "Zes"];
+var numberToWord = ["Een", "Twee", "Drie", "Vier", "Vijf", "Zes"];
 var counts = [0, 0, 0, 0, 0, 0];
+var numArray = [0, 0, 0, 0, 0, 0, 0, 0];
+numArray = numArray.sort();
 
-function myfunction(max) {
-    let diceroll = Math.floor(Math.random() * Math.floor(max)) + 1;
-    document.getElementById("rollresult").innerHTML = diceroll;
+function gooi(max) {
+    let diceRoll = Math.floor(Math.random() * Math.floor(max)) + 1;
+    document.getElementById("rollresult").innerHTML = diceRoll;
     console.log("Er is gegooid");
-    console.log(diceroll);
-    document.getElementById("rollresultword").innerHTML = numbertoword[diceroll - 1];
-    counts[diceroll - 1]++;
-    document.getElementById("roll" + diceroll).innerHTML = counts[diceroll - 1];
+    console.log(diceRoll);
+    document.getElementById("rollresultword").innerHTML = numberToWord[diceRoll - 1];
+    counts[diceRoll - 1]++;
+    document.getElementById("roll" + diceRoll).innerHTML = counts[diceRoll - 1];
+}
+
+function gooi8(max) {
+    function theRoll(max, list) {
+        let diceRoll = Math.floor(Math.random() * Math.floor(max)) + 1;
+        counts[diceRoll - 1]++;
+        document.getElementById("rolle" + diceRoll).innerHTML = counts[diceRoll - 1];
+        numArray[list - 1] = " " + diceRoll;
+    }
+    theRoll(max, 1);
+    theRoll(max, 2);
+    theRoll(max, 3);
+    theRoll(max, 4);
+    theRoll(max, 5);
+    theRoll(max, 6);
+    theRoll(max, 7);
+    theRoll(max, 8);
+    numArray = numArray.sort();
+    document.getElementById("rolleresultword").innerHTML = numArray;
+    console.log("Er is gegooid");
+    console.log(numArray);
 }
 
 function mySecondfunction(max) {
-    let diceroll = Math.floor(Math.random() * Math.floor(max)) + 1;
-    document.getElementById("rollresult2").innerHTML = diceroll;
+    let diceRoll = Math.floor(Math.random() * Math.floor(max)) + 1;
+    document.getElementById("rollresult2").innerHTML = diceRoll;
     console.log("Er is gegooid");
-    console.log(diceroll);
+    console.log(diceRoll);
 }
